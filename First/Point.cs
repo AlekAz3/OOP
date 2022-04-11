eec42984e76db88358da916acba816d9770d4b5f
Alek <vakarin2020@gmail.com>
2022-04-11 22:10:00 +0300

    Добавьте файлы проекта.

---
+++ First/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    internal class Point
+    {
+        private double x;
+        private double y;
+
+        public double X { get { return x; } set { x = value; } }
+        public double Y { get { return y; } set { y = value; } }
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double FindDistance(Point point) => Math.Sqrt(Math.Pow(point.X-this.X, 2) + Math.Pow(point.Y - this.Y, 2));
+        
+    }
+}
---
+++ First/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point(-1, 3);
+            Point point2 = new Point(6, 2);
+            Console.WriteLine(point1.FindDistance(point2));
+        }
+    }
+}
