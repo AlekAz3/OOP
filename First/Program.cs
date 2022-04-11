1b0d9578788e36cef17b5d93c0906f830858277d
Alek <vakarin2020@gmail.com>
2022-04-11 22:11:03 +0300

    Первое задание готово

--- First/Program.cs
+++ First/Program.cs
@@ -8,6 +8,7 @@ namespace First
         {
             Point point1 = new Point(-1, 3);
             Point point2 = new Point(6, 2);
+
             Console.WriteLine(point1.FindDistance(point2));
         }
     }
