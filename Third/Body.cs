3be7f4b312f4d1665bda922499d8cf0ebe23bf5e
Alek <vakarin2020@gmail.com>
2022-04-11 22:33:23 +0300

    Третье задание Готово

---
+++ Third/Body.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Third
+{
+    internal class Body
+    {
+        private decimal weight;
+        private decimal speed;
+
+        public decimal Weight
+        {
+            get { return weight; }
+            set 
+            {
+                if (value < 0)
+                    throw new ArgumentException("Значение меньше 0");
+                else 
+                    weight = value;
+            }
+        }
+
+        public decimal Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Значение меньше 0");
+                else
+                    speed = value;
+            }
+        }
+
+        public Body(decimal weight, decimal speed)
+        {
+            this.Weight = weight;
+            this.Speed = speed;
+        }
+
+        public decimal FindKineticEnergy() => (this.weight*this.speed*this.speed) / 2;
+
+    }
+}
