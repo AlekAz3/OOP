7c48d3a772c08ce06a899da8802300ab3ee7cd91
Alek <vakarin2020@gmail.com>
2022-04-16 08:07:33 +0300

    Форма без функционала

--- View/Form1.Designer.cs
+++ View/Form1.Designer.cs
@@ -28,13 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ChooseAction = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.do_button = new System.Windows.Forms.Button();
+            this.acs = new System.Windows.Forms.ComboBox();
+            this.FillButton = new System.Windows.Forms.Button();
+            this.Arr1NTextBox = new System.Windows.Forms.TextBox();
+            this.Arr1MTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Arr2NTextBox = new System.Windows.Forms.TextBox();
+            this.Arr2MTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChooseAction
+            // 
+            this.ChooseAction.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ChooseAction.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ChooseAction.FormattingEnabled = true;
+            this.ChooseAction.Items.AddRange(new object[] {
+            "1. Нахождение расстояния между двумя точками на плоскости.",
+            "2. Сложение или умножение комплесксных чисел.",
+            "3. Вычислить энергию тела, если известны его масса и скорость.",
+            "4. Умножить матрицы"});
+            this.ChooseAction.Location = new System.Drawing.Point(13, 13);
+            this.ChooseAction.Name = "ChooseAction";
+            this.ChooseAction.Size = new System.Drawing.Size(521, 26);
+            this.ChooseAction.TabIndex = 0;
+            this.ChooseAction.SelectedIndexChanged += new System.EventHandler(this.ChooseAction_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(300, 300);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(350, 82);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(300, 300);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(679, 82);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(300, 300);
+            this.dataGridView3.TabIndex = 1;
+            // 
+            // do_button
+            // 
+            this.do_button.Location = new System.Drawing.Point(198, 430);
+            this.do_button.Name = "do_button";
+            this.do_button.Size = new System.Drawing.Size(96, 33);
+            this.do_button.TabIndex = 2;
+            this.do_button.Text = "Выполнить ";
+            this.do_button.UseVisualStyleBackColor = true;
+            // 
+            // acs
+            // 
+            this.acs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.acs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.acs.FormattingEnabled = true;
+            this.acs.Items.AddRange(new object[] {
+            "1. Сложить",
+            "2. Умножить "});
+            this.acs.Location = new System.Drawing.Point(13, 435);
+            this.acs.Name = "acs";
+            this.acs.Size = new System.Drawing.Size(156, 24);
+            this.acs.TabIndex = 3;
+            // 
+            // FillButton
+            // 
+            this.FillButton.Location = new System.Drawing.Point(12, 45);
+            this.FillButton.Name = "FillButton";
+            this.FillButton.Size = new System.Drawing.Size(96, 33);
+            this.FillButton.TabIndex = 2;
+            this.FillButton.Text = "Заполнить";
+            this.FillButton.UseVisualStyleBackColor = true;
+            this.FillButton.Click += new System.EventHandler(this.FillButton_Click);
+            // 
+            // Arr1NTextBox
+            // 
+            this.Arr1NTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Arr1NTextBox.Location = new System.Drawing.Point(119, 389);
+            this.Arr1NTextBox.Name = "Arr1NTextBox";
+            this.Arr1NTextBox.Size = new System.Drawing.Size(30, 24);
+            this.Arr1NTextBox.TabIndex = 4;
+            // 
+            // Arr1MTextBox
+            // 
+            this.Arr1MTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Arr1MTextBox.Location = new System.Drawing.Point(155, 389);
+            this.Arr1MTextBox.Name = "Arr1MTextBox";
+            this.Arr1MTextBox.Size = new System.Drawing.Size(30, 24);
+            this.Arr1MTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 393);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Размерность";
+            // 
+            // Arr2NTextBox
+            // 
+            this.Arr2NTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Arr2NTextBox.Location = new System.Drawing.Point(454, 391);
+            this.Arr2NTextBox.Name = "Arr2NTextBox";
+            this.Arr2NTextBox.Size = new System.Drawing.Size(30, 24);
+            this.Arr2NTextBox.TabIndex = 4;
+            // 
+            // Arr2MTextBox
+            // 
+            this.Arr2MTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Arr2MTextBox.Location = new System.Drawing.Point(490, 391);
+            this.Arr2MTextBox.Name = "Arr2MTextBox";
+            this.Arr2MTextBox.Size = new System.Drawing.Size(30, 24);
+            this.Arr2MTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(347, 395);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Размерность";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1006, 475);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Arr2MTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Arr2NTextBox);
+            this.Controls.Add(this.Arr1MTextBox);
+            this.Controls.Add(this.Arr1NTextBox);
+            this.Controls.Add(this.acs);
+            this.Controls.Add(this.FillButton);
+            this.Controls.Add(this.do_button);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ChooseAction);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Объектно-ориентированное программирование";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox ChooseAction;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button do_button;
+        private System.Windows.Forms.ComboBox acs;
+        private System.Windows.Forms.Button FillButton;
+        private System.Windows.Forms.TextBox Arr1NTextBox;
+        private System.Windows.Forms.TextBox Arr1MTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Arr2NTextBox;
+        private System.Windows.Forms.TextBox Arr2MTextBox;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- View/Form1.cs
+++ View/Form1.cs
@@ -7,14 +7,227 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tasks;
 
 namespace View
 {
     public partial class Form1 : Form
     {
+        //Первое
+        private Label label1;
+        private Label label2;
+        private Label RezultLabel;
+        private TextBox x1TextBox;
+        private TextBox y1TextBox;
+        private TextBox x2TextBox;
+        private TextBox y2TextBox;
+        private Button ShowResultBtn;
+
+        // Матрица
+        private Matrix matrix1;
+        private Matrix matrix2;
+        private Matrix matrix3;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ChooseAction.SelectedIndex = 0;
+            acs.SelectedIndex = 0;
+        }
+
+        private void ShowFirst()
+        {
+            // 
+            // label1
+            // 
+            this.label1 = new Label();
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Координаты первой точки";
+            // 
+            // x1TextBox
+            // 
+            this.x1TextBox = new TextBox();
+            this.x1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x1TextBox.Location = new System.Drawing.Point(212, 55);
+            this.x1TextBox.Name = "x1TextBox";
+            this.x1TextBox.Size = new System.Drawing.Size(57, 24);
+            this.x1TextBox.TabIndex = 2;
+            // 
+            // y1TextBox
+            // 
+            this.y1TextBox = new TextBox();
+            this.y1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.y1TextBox.Location = new System.Drawing.Point(275, 55);
+            this.y1TextBox.Name = "y1TextBox";
+            this.y1TextBox.Size = new System.Drawing.Size(57, 24);
+            this.y1TextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2 = new Label();
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Координаты второй точки";
+            // 
+            // x2TextBox
+            // 
+            this.x2TextBox = new TextBox();
+            this.x2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x2TextBox.Location = new System.Drawing.Point(212, 89);
+            this.x2TextBox.Name = "x2TextBox";
+            this.x2TextBox.Size = new System.Drawing.Size(57, 24);
+            this.x2TextBox.TabIndex = 2;
+            // 
+            // y2TextBox
+            // 
+            this.y2TextBox = new TextBox();
+            this.y2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.y2TextBox.Location = new System.Drawing.Point(275, 89);
+            this.y2TextBox.Name = "y2TextBox";
+            this.y2TextBox.Size = new System.Drawing.Size(57, 24);
+            this.y2TextBox.TabIndex = 2;
+            // 
+            // ShowResultBtn
+            // 
+            this.ShowResultBtn = new Button();
+            this.ShowResultBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ShowResultBtn.Location = new System.Drawing.Point(13, 124);
+            this.ShowResultBtn.Name = "ShowResultBtn";
+            this.ShowResultBtn.Size = new System.Drawing.Size(192, 38);
+            this.ShowResultBtn.TabIndex = 3;
+            this.ShowResultBtn.Text = "Рассчитать";
+            this.ShowResultBtn.UseVisualStyleBackColor = true;
+            this.ShowResultBtn.Click += new System.EventHandler(this.FirstResultButton_Click);
+            // 
+            // RezultLabel
+            // 
+            this.RezultLabel = new Label();
+            this.RezultLabel.AutoSize = true;
+            this.RezultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RezultLabel.Location = new System.Drawing.Point(13, 184);
+            this.RezultLabel.Name = "RezultLabel";
+            this.RezultLabel.Size = new System.Drawing.Size(0, 25);
+            this.RezultLabel.TabIndex = 1;
+
+
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RezultLabel);
+
+            this.Controls.Add(this.x1TextBox);
+            this.Controls.Add(this.y1TextBox);
+            this.Controls.Add(this.x2TextBox);
+            this.Controls.Add(this.y2TextBox);
+            this.Controls.Add(this.ShowResultBtn);
+    }
+
+        private void HideFirst()
+        {
+            if (label1 == null)
+                return;
+            
+            label1.Dispose();
+            label2.Dispose();
+            RezultLabel.Dispose();
+            x1TextBox.Dispose();
+            y1TextBox.Dispose();
+            x2TextBox.Dispose();
+            y2TextBox.Dispose();
+            ShowResultBtn.Dispose();
+        }
+
+
+        private void ShowSecond() 
+        {
+            dataGridView1.Show();
+            dataGridView2.Show();
+            dataGridView3.Show();
+            acs.Show();
+            do_button.Show();
+            FillButton.Show();
+            label3.Show();
+            label4.Show();
+            Arr1MTextBox.Show();
+            Arr1NTextBox.Show();
+            Arr2NTextBox.Show();
+            Arr2MTextBox.Show();
+
+        }
+
+        private void HideSecond()
+        {
+            dataGridView1.Hide();
+            dataGridView2.Hide();
+            dataGridView3.Hide();
+            acs.Hide();
+            do_button.Hide();
+            FillButton.Hide();
+            Arr1MTextBox.Hide();
+            Arr1NTextBox.Hide();
+            Arr2NTextBox.Hide();
+            Arr2MTextBox.Hide();
+        }
+
+
+        public void WriteData(DataGridView dataGridView, Matrix matrix)
+        {
+            dataGridView.RowCount = matrix.N;
+            dataGridView.ColumnCount = matrix.M;
+            for (int i = 0; i < matrix.N; i++)
+                for (int j = 0; j < matrix.M; j++)
+                    dataGridView.Rows[i].Cells[j].Value = matrix.Matrixx[i, j];
+        }
+
+
+
+        public void FirstResultButton_Click(object sender, EventArgs e)
+        {
+            Tasks.Point point1 = new Tasks.Point(Convert.ToDouble(x1TextBox.Text), Convert.ToDouble(y1TextBox.Text));
+            Tasks.Point point2 = new Tasks.Point(Convert.ToDouble(x2TextBox.Text), Convert.ToDouble(y2TextBox.Text));
+            RezultLabel.Text = Tasks.Point.FindDistance(point1, point2).ToString();
+        }
+
+        private void ChooseAction_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (ChooseAction.SelectedIndex)
+            {
+                case 0:
+                    ShowFirst();
+                    HideSecond();
+                    break;
+                case 1:
+                    HideFirst();
+                    HideSecond();
+                    break;
+                case 2:
+                    HideFirst();
+                    HideSecond();
+                    break;
+                case 3:
+                    HideFirst();
+                    ShowSecond();
+                    break;
+
+            }
+        }
+
+        private void FillButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
